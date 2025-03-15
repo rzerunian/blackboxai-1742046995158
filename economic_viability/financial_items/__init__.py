d60869702f93baf4e681eb769a3e89e8b8fec0bc
Rafael Zerunian <41594403+rzerunian@users.noreply.github.com>
2025-03-15 10:56:40 -0300

    Add economic_viability/financial_items/__init__.py 1742047008057

---
+++ economic_viability/financial_items/__init__.py
@@ -0,0 +1,14 @@
+from .base_item import BaseFinancialItem
+from .capex import CapExItem, CapexManager
+from .opex import OpExItem, OpexManager
+from .receitas import ReceitaItem, ReceitasManager
+
+__all__ = [
+    'BaseFinancialItem',
+    'CapExItem',
+    'CapexManager',
+    'OpExItem',
+    'OpexManager',
+    'ReceitaItem',
+    'ReceitasManager'
+]
